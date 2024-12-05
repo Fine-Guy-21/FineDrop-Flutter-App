91d6664a96e0e043abf1d58a3aefec2f66cc929b
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2024-12-05 21:53:52 +0300

    Update #2
    Home Page Statistics Bar added
    user some attributes added

--- airdrop_demo/lib/Pages/home.dart
+++ airdrop_demo/lib/Pages/home.dart
@@ -32,13 +32,81 @@ class _HomePageState extends State<HomePage> {
                 margin: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue[800]),
+                    color: Colors.blue[900]),
                 child:Text(
                   'Hello, ${_user.userName}',
                   style:const TextStyle(fontSize: 20, color: Colors.white),
                 ),
               ),
-            )
+            ),
+
+            const SizedBox(height: 5),
+            // Statistics Bar 
+            Center(
+              child:Container(
+                  margin: const EdgeInsets.all(6),
+                  padding: const EdgeInsets.all(6),  
+                  height: screenHeight * (15/100),
+                  width: screenWidth,                 
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue[900],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                                Container(
+                                    width: screenWidth*(30/100),
+                                    height: 50.0,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue[600],
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    child: Text(
+                                      "Points Per Tap \n ${_user.pointPerTap}",
+                                      textAlign: TextAlign.center,
+                                      style:const TextStyle(
+                                        fontSize: 14,
+                                      ),  
+                                    ),
+                                  ),
+                                  
+                                  Container(
+                                    width: screenWidth*(30/100),
+                                    height: 50.0,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue[600],
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    child: Text(
+                                      "Next level \n ${_user.nextLevel}",
+                                      textAlign: TextAlign.center,
+                                      style:const TextStyle(
+                                        fontSize: 14,
+                                      ),  
+                                    ),
+                                  ),
+                                  
+                                  Container(
+                                    width: screenWidth*(30/100),
+                                    height: 50.0,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue[600],
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    child:  Text(
+                                      "Profit per hour \n ${_user.profitPerHour}",
+                                      textAlign: TextAlign.center,
+                                      style:const TextStyle(
+                                        fontSize: 14,
+                                      ),  
+                                    ),
+                                  ),
+                              ],
+                  ),
+                ),
+            ),
+
           ],
         ),
       ),
--- airdrop_demo/lib/model/user.dart
+++ airdrop_demo/lib/model/user.dart
@@ -2,5 +2,14 @@
 class UserProfile {
   
   String? userName;
+  int pointPerTap = 1;
+  int userPoints = 0;
+  int nextLevel = 5000;
+  int profitPerHour = 0;
+ void pointsForNextLevel() {
+
+  nextLevel =  nextLevel * 5; 
+  }
+
   
 }
